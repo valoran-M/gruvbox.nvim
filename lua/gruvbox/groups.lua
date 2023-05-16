7e1f884559b1a8bc1f5cad9f1b73c7fdff76e794
valomay <valomay@protonmail.com>
2023-05-16 16:40:28 +0200

    change blankline

--- lua/gruvbox/groups.lua
+++ lua/gruvbox/groups.lua
@@ -985,7 +985,7 @@ M.setup = function()
     TSRainbowCyan = { fg = colors.cyan },
 
     -- ident blankline
-    IndentBlanklineIndent1 = { fg = colors.bg1 },
+    IndentBlanklineIndent = { fg = colors.bg1 },
     IndentBlanklineContextChar = { fg = colors.neutral_green },
   }
 
